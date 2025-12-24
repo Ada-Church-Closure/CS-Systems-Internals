83c51eddf65c78d93909c61bd75ae51d59809cb8
Ada-Church-Closure <2574094394@qq.com>
2025-12-24 22:51:13 +0800

    Stop tracking CS144 folder directly

--- CS144-minnow/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ada/Lessons/CS144-minnow/src
-# Build directory: /home/ada/Lessons/CS144-minnow/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
